9bfc43c6a80a3752a8c812ecfca7722b9f4c086e
dat guy <1834tj@vanguardcharter.org>
2025-09-22 09:12:01 -0600

    this is all of my class work

---
+++ lecture examples/week 7.py
@@ -0,0 +1,26 @@
+def main() -> None:
+
+    f = open"../Lecture ExampleDota/test-file1.txt", "r", encoding="utf8")
+
+    file_input = f.read()
+    file_input_lower: str = file_input.lower()
+    counter: int = file_input_lower.count("this", _start:0, len(file_input_lower))
+
+    print(counter)
+
+if __name__ == "__main__":
+    main()
+
+
+def main() -> None:
+        print("Check out this cool book reader app!")
+        text_file: str = input("What file do you want to proccess? ")
+        try:
+            with open(f"../Lecture ExamplesData/{text_file}.txt", "r") as file:
+                text = file.read()
+                print(text)
+            except FileNotFoundError:
+            print("File not found")
+
+if __name__ == "__main__":
+    main()
