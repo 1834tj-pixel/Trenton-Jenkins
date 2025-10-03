46d22fa119c47b6d320c11023486db4c48ada762
dat guy <1834tj@vanguardcharter.org>
2025-10-03 09:18:00 -0600

    this is all of my class work

---
+++ lecture examples/week 8 examples.py
@@ -0,0 +1,18 @@
+def main() -> None
+    for n in rang(0, num_of_passwords):
+        otions = []
+        user_input = input("Do you want lower-cased letters? [yes]: ") lower()
+        options.append(True if user_input == "yes" or (user_input == "y") else False)
+
+        user_input = input("Do you want upper-cased letters? [yes]: ") lower()
+        options
+
+        user_input = input("Do you want numbers? [yes]: ")
+        if user_input == "yes" or user_input == "y":
+            optional_characters. extend(String.punctuation)
+
+        for num in range(0, num_of_chars):
+            print(randon.choice(optional_characters))
+
+
+
